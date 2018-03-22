3d2af8160ea084693bf2a3a35ece04228e586787
otsha <32258613+otsha@users.noreply.github.com>
2018-03-22 13:20:20 +0200

    Write tests for Maksukortti

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,39 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoMuuttujaAlussaOikein() {
+        assertTrue(kortti.saldo()==10);
+    }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(150);
+        assertEquals("saldo: 1.60", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaOnTarpeeksi() {
+        kortti.otaRahaa(9);
+        assertEquals("saldo: 0.01", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        kortti.otaRahaa(200);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void palautetaanFalseJosRahatEivatRiittaneet() {
+        assertTrue(kortti.otaRahaa(200) == false);
+    }
+    
+    
 }
