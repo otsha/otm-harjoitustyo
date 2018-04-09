896bdca97a4bb9fb6fca5dd893465d7f2b50581e
otsha <32258613+otsha@users.noreply.github.com>
2018-04-09 12:47:43 +0300

    Remove some pre-created code

--- OTM_BudgetingApp/src/main/java/otsha/otm_budgetingapp/MainApp.java
+++ OTM_BudgetingApp/src/main/java/otsha/otm_budgetingapp/MainApp.java
@@ -2,9 +2,8 @@ package otsha.otm_budgetingapp;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
@@ -12,24 +11,16 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        BorderPane view = new BorderPane();
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(view);
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("BudgetingApp");
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
