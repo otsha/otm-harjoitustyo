5ca3f0cd0f225e27723fba349d7f2e6872226267
otsha <32258613+otsha@users.noreply.github.com>
2018-04-14 13:14:40 +0300

    Refactor UI

--- OTM_BudgetingApp/src/main/java/ui/SceneController.java
+++ OTM_BudgetingApp/src/main/java/ui/SceneController.java
@@ -5,6 +5,8 @@ import data.Database;
 import data.Plan;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -24,120 +26,33 @@ public class SceneController {
     private double height;
     private Stage stage;
     private Database db;
+    private Insets insets;
 
     public SceneController(Stage s, Database db) {
         this.width = 640;
         this.height = 480;
         this.stage = s;
         this.db = db;
+        this.insets = new Insets(20, 20, 20, 20);
+    }
+    
+    // Setup an empty default view
+    public BorderPane setup() {
+        BorderPane view = new BorderPane();
+        view.setPrefWidth(width);
+        view.setPrefHeight(height);
+        view.setPadding(insets);
+        
+        return view;
     }
     
     // THIS IS SHOWN WHEN THE APP FIRST STARTS
-    public void initialScene() {
-
-        // Setup the initial scene
-        BorderPane initialView = new BorderPane();
-        initialView.setPrefWidth(width);
-        initialView.setPrefHeight(height);
-        initialView.setPadding(new Insets(20, 20, 20, 20));
-        Scene initialScene = new Scene(initialView);
-
-        // Setup the options for creating a plan and opening an existing one
-        VBox mainMenu = new VBox();
-
-        Label initLabel = new Label("Hello!");
-        Button createPlan = new Button("Create a new budget plan");
-        Button openPlan = new Button("Open an existing budget plan");
-
-        // Handle pressing the 'Create plan' button
-        createPlan.setOnAction((event) -> {
-            createPlan();
-        });
-
-        // Handle pressing the 'Open a plan' button
-        openPlan.setOnAction((event) -> {
-            try {
-                openPlan();
-            } catch (SQLException ex) {
-                System.out.println("error");
-            }
-        });
-
-        mainMenu.getChildren().addAll(initLabel, createPlan, openPlan);
-        initialView.setCenter(mainMenu);
-
-        // initialScene.getStylesheets().add("/styles/Styles.css");
-        stage.setScene(initialScene);
-    }
-    
-    // CREATE A NEW BUDGET PLAN
-    public void createPlan() {
+    // Managing (Creating, opening and deleting) budget plans
+    public void initialScene() throws SQLException {
         PlanDao pDao = new PlanDao(db);
 
-        // Setup the scene
-        BorderPane view = new BorderPane();
-        view.setPrefWidth(width);
-        view.setPrefHeight(height);
-        view.setPadding(new Insets(20, 20, 20, 20));
+        BorderPane view = setup();
         Scene scene = new Scene(view);
-
-        // Add a back button
-        Button backToMainMenu = new Button("<");
-        backToMainMenu.setOnAction((event) -> {
-            initialScene();
-        });
-        view.setTop(backToMainMenu);
-
-        // Setup the budget plan creation form
-        VBox formCreatePlan = new VBox();
-
-        Label inputErrorLabel = new Label("");
-        Label planNameLabel = new Label("Name your plan:");
-        TextField planName = new TextField();
-        Label budgetAmountLabel = new Label("What's your budget?");
-        TextField budgetAmount = new TextField();
-        Button createNewPlan = new Button("Create");
-
-        // Handle creating a new plan
-        createNewPlan.setOnAction((event) -> {
-            if (planName.getText().equals("") || budgetAmount.getText().equals("")) {
-                inputErrorLabel.setText("The name and/or the budget amount cannot be empty.");
-            } else {
-                Plan p = new Plan(0, planName.getText(), Double.parseDouble(budgetAmount.getText()));
-
-                try {
-                    pDao.saveOrUpdate(p);
-                } catch (SQLException ex) {
-
-                }
-
-                editPlan(p);
-            }
-        });
-
-        formCreatePlan.getChildren().addAll(inputErrorLabel, planNameLabel, planName, budgetAmountLabel, budgetAmount, createNewPlan);
-        view.setCenter(formCreatePlan);
-
-        stage.setScene(scene);
-    }
-    
-    // OPENING AND/OR DELETING AN EXISTING BUDGET PLAN
-    public void openPlan() throws SQLException {
-        PlanDao pDao = new PlanDao(db);
-
-        // Setup the scene
-        BorderPane view = new BorderPane();
-        view.setPrefWidth(width);
-        view.setPrefHeight(height);
-        view.setPadding(new Insets(20, 20, 20, 20));
-        Scene scene = new Scene(view);
-
-        // Add a back button
-        Button backToMainMenu = new Button("<");
-        backToMainMenu.setOnAction((event) -> {
-            initialScene();
-        });
-        view.setTop(backToMainMenu);
 
         // Setup the plan opening view
         VBox openingMenu = new VBox();
@@ -175,14 +90,20 @@ public class SceneController {
                 Plan p = pDao.findOneByName(planListView.getSelectionModel().selectedItemProperty().getValue());
                 pDao.delete(p.getId());
                 // Refresh the scene after deleting an item to show the updated listing
-                openPlan();
+                initialScene();
             } catch (SQLException ex) {
                 System.out.println("error");
             }
 
         });
+        
+        // Creating a new plan
+        Button createPlan = new Button("New...");
+        createPlan.setOnAction((event) -> {
+            createPlan();
+        }); 
 
-        openOrDelete.getChildren().addAll(openPlan, deletePlan);
+        openOrDelete.getChildren().addAll(createPlan, openPlan, deletePlan);
 
         openingMenu.getChildren().addAll(planListView, openOrDelete);
 
@@ -191,19 +112,71 @@ public class SceneController {
         stage.setScene(scene);
     }
     
-    // VIEW THE DETAILS OF A PLAN
-    public void editPlan(Plan p) {
-        // Setup the scene
-        BorderPane view = new BorderPane();
-        view.setPrefWidth(width);
-        view.setPrefHeight(height);
-        view.setPadding(new Insets(20, 20, 20, 20));
+    // CREATE A NEW BUDGET PLAN
+    public void createPlan() {
+        PlanDao pDao = new PlanDao(db);
+
+        BorderPane view = setup();
         Scene scene = new Scene(view);
 
         // Add a back button
         Button backToMainMenu = new Button("<");
         backToMainMenu.setOnAction((event) -> {
-            initialScene();
+            try {
+                initialScene();
+            } catch (SQLException ex) {
+                System.out.println("error");
+            }
+        });
+        view.setTop(backToMainMenu);
+
+        // Setup the budget plan creation form
+        VBox formCreatePlan = new VBox();
+
+        Label inputErrorLabel = new Label("");
+        Label planNameLabel = new Label("Name your plan:");
+        TextField planName = new TextField();
+        Label budgetAmountLabel = new Label("What's your budget?");
+        TextField budgetAmount = new TextField();
+        Button createNewPlan = new Button("Create");
+
+        // Handle creating a new plan
+        createNewPlan.setOnAction((event) -> {
+            if (planName.getText().equals("") || budgetAmount.getText().equals("")) {
+                inputErrorLabel.setText("The name and/or the budget amount cannot be empty.");
+            } else {
+                Plan p = new Plan(0, planName.getText(), Double.parseDouble(budgetAmount.getText()));
+
+                try {
+                    pDao.saveOrUpdate(p);
+                } catch (SQLException ex) {
+
+                }
+
+                editPlan(p);
+            }
+        });
+
+        formCreatePlan.getChildren().addAll(inputErrorLabel, planNameLabel, planName, budgetAmountLabel, budgetAmount, createNewPlan);
+        view.setCenter(formCreatePlan);
+
+        stage.setScene(scene);
+    }
+    
+    // VIEW THE DETAILS OF A PLAN
+    public void editPlan(Plan p) {
+
+        BorderPane view = setup();
+        Scene scene = new Scene(view);
+
+        // Add a back button
+        Button backToMainMenu = new Button("<");
+        backToMainMenu.setOnAction((event) -> {
+            try {
+                initialScene();
+            } catch (SQLException ex) {
+                System.out.println("error");
+            }
         });
         view.setTop(backToMainMenu);
 
