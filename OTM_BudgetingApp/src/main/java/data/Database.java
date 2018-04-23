a6bde623e202f530d3967e928222af5aabf20d31
otsha <32258613+otsha@users.noreply.github.com>
2018-04-23 14:36:48 +0300

    Database now initializes the required tables if necessary

--- OTM_BudgetingApp/src/main/java/data/Database.java
+++ OTM_BudgetingApp/src/main/java/data/Database.java
@@ -2,6 +2,7 @@ package data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database {
@@ -10,9 +11,76 @@ public class Database {
 
     public Database(String databaseAddress) {
         this.address = databaseAddress;
+        initTables();
     }
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(address);
     }
+    
+    private void initTables() {
+        // The following methods will create required tables if they do not already exist in the database
+        initPlans();
+        initCategories();
+        initExpenses();
+    }
+
+    private void initPlans() {
+        try {
+            Connection conn = getConnection();
+
+            PreparedStatement createPlanTable = conn.prepareStatement("CREATE TABLE IF NOT EXISTS Plan ("
+                    + "id integer PRIMARY KEY, "
+                    + "name varchar(255), "
+                    + "budget float);"
+            );
+            createPlanTable.execute();
+            createPlanTable.close();
+
+            conn.close();
+        } catch (SQLException e) {
+
+        }
+    }
+
+    private void initCategories() {
+        try {
+            Connection conn = getConnection();
+
+            PreparedStatement createCategoryTable = conn.prepareStatement("CREATE TABLE IF NOT EXISTS Category ("
+                    + "id integer PRIMARY KEY, "
+                    + "plan_id integer, "
+                    + "name varchar(255), "
+                    + "allocated float, "
+                    + "FOREIGN KEY (plan_id) REFERENCES Plan(id));"
+            );
+            createCategoryTable.execute();
+            createCategoryTable.close();
+            
+            conn.close();
+        } catch (SQLException e) {
+
+        }
+    }
+    
+    private void initExpenses() {
+        try {
+            Connection conn = getConnection();
+
+            PreparedStatement createExpenseTable = conn.prepareStatement("CREATE TABLE IF NOT EXISTS Expense ("
+                    + "id integer PRIMARY KEY, "
+                    + "category_id integer, "
+                    + "name varchar(255), "
+                    + "amount float, "
+                    + "FOREIGN KEY (category_id) REFERENCES Category(id));"
+            );
+            createExpenseTable.execute();
+            createExpenseTable.close();
+            
+            conn.close();
+        } catch (SQLException e) {
+
+        }
+    }
+
 }
